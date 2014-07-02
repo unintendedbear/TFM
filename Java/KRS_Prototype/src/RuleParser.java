8e3de36801061d1a40bf825d90756607ada907fe
unintendedbear <unintendedbear@gmail.com>
2014-07-02 10:44:04 +0200

    avanzando un poco RuleParser.java

--- Java/KRS_Prototype/src/RuleParser.java
+++ Java/KRS_Prototype/src/RuleParser.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.List;
+import java.util.ArrayList;
 
 public class RuleParser {
 
@@ -14,12 +16,14 @@ public class RuleParser {
 		String argumentsPattern1 = "\\s*(.*)(==)\"(.+)\"";
 		String argumentsPattern2 = "\\s*(.+)([>|<|=])(\\d+)";
 		String argumentsPattern3 = "\\s*(.+) (.+) \"?\\*\\.(.+)\\.\\*\"?";
-		
+				
 		try {
 			
 			// Leer DRL
 			ReadFile file = new ReadFile(drlFile);
 			String[] aryLines = file.OpenFile();
+			List<Rule> myList = new ArrayList<Rule>();
+			
 			
 			int i;
 			for ( i=0; i < aryLines.length; i++ ) {
@@ -28,7 +32,9 @@ public class RuleParser {
 				Matcher matcherLine = patternOfCondition.matcher(aryLines[i]);
 				
 				if (matcherLine.find()) {
-					System.out.println("Found value: " + matcherLine.group(1) );
+					// Condiciones en matcherLine.group(1)
+					
+					String[] arrayConditions = matcherLine.group(1).split(",");
 					
 					Pattern patternOfArgument1 = Pattern.compile(argumentsPattern1);
 					Pattern patternOfArgument2 = Pattern.compile(argumentsPattern2);
@@ -38,27 +44,43 @@ public class RuleParser {
 					Matcher matcherArguments2 = patternOfArgument2.matcher(matcherLine.group(1));
 					Matcher matcherArguments3 = patternOfArgument3.matcher(matcherLine.group(1));
 					
-					if (matcherArguments1.find()) {
-						System.out.println("Found value: " + matcherArguments1.group(1) );
-						System.out.println("Found value: " + matcherArguments1.group(2) );
-						System.out.println("Found value: " + matcherArguments1.group(3) );
-					} else if(matcherArguments2.find()) {
-						System.out.println("Found value: " + matcherArguments2.group(1) );
-						System.out.println("Found value: " + matcherArguments2.group(2) );
-						System.out.println("Found value: " + matcherArguments2.group(3) );						
-					} else if(matcherArguments3.find()) {
-						System.out.println("Found value: " + matcherArguments3.group(1) );
-						System.out.println("Found value: " + matcherArguments3.group(2) );
-						System.out.println("Found value: " + matcherArguments3.group(3) );						
+					if (arrayConditions.length > 1) {
+						
+					} else {
+						if (matcherArguments1.find()) {
+							System.out.println("Found value: " + matcherArguments1.group(1) );
+							System.out.println("Found value: " + matcherArguments1.group(2) );
+							System.out.println("Found value: " + matcherArguments1.group(3) );
+						} else if(matcherArguments2.find()) {
+							System.out.println("Found value: " + matcherArguments2.group(1) );
+							System.out.println("Found value: " + matcherArguments2.group(2) );
+							System.out.println("Found value: " + matcherArguments2.group(3) );						
+						} else if(matcherArguments3.find()) {
+							System.out.println("Found value: " + matcherArguments3.group(1) );
+							System.out.println("Found value: " + matcherArguments3.group(2) );
+							System.out.println("Found value: " + matcherArguments3.group(3) );						
+						}
 					}
+					
+						
 				}
 			}
 		}
 		catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-        
+		        
         
     }
+	
+	public String[] compare(Pattern pattern, Matcher matcher, int size) {
+		
+		int arraySize = size*3;
+		String[] arrayArguments = new String[arraySize];
+		
+		
+		
+		return arrayArguments;
+	}
 
 }
