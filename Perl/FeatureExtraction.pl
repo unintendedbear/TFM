fb8197f810020aee59b5136227c2de74a32fd47b
unintendedbear <unintendedbear@gmail.com>
2014-12-02 13:43:33 +0100

    script para el análisis de reglas de weka

---
+++ Perl/FeatureExtraction.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use File::Slurp qw(read_file);
+use Carp qw(croak);
+use Switch;
+
+my $file = shift || croak "Please specify a file.";
+
+my @rules = read_file( $file );
+my %features;
+my $instances;
+my $classifier;
+my @partfeatures = ();
+
+for my $rule ( @rules ) {
+
+	if ( $rule =~ /(.*)\n/ ) {
+		$rule = $1;
+	}
+
+	if ( $rule =~ /Scheme:\s*([\w\.]+)/ ) { #Which classifier?
+		my @attributes = split (/\./, $1);
+		$classifier = $attributes[$#attributes];
+	}
+
+	if ( $rule =~ /Instances:\s+(\d+)/ ) { #Number of instances
+		$instances = $1;
+	}
+
+	switch ($classifier) {
+
+		case "JRip" {
+			######## JRIP ########
+			if ( $rule =~ /^\(\w+[\s\>\=\<]+[\w\.]+\)/ ) {
+				my @components = split (/\sand\s/, $rule);
+				my $weight;
+				my @subcomponents = split (/\s/, $components[$#components]);
+				if ( $subcomponents[$#subcomponents] =~ /\((\d+)\.\d+\/\d+\.\d+\)/ ) {
+					$weight = $1/$instances;
+				}
+				for my $component ( @components ) {
+					if ( $component =~ /^\((\w+)[\s\>\=\<]+[\w\.]+\)/ ) {
+						$features{$1} += $weight;
+					}
+				}
+			}
+		}
+		case "PART" {
+			######## PART ########
+			if ( $rule =~ /^(\w+)[\s\>\=\<]+\w+\sAND$/ ) {
+				print "$rule\n";
+				push (@partfeatures, $1);
+			}
+
+			if ( $rule =~ /^(\w+)[\s\>\=\<]+\w+\:\s\w+\s\((\d+)\.\d+\/?\d*\.*\d*\)$/ ) {
+				print "$rule\n";
+				push (@partfeatures, $1);
+				my $weight = $2/$instances;
+
+				for my $i ( @partfeatures ) {
+					print "$i,";
+					$features{$i} += $weight;
+				}
+				print "\n";
+				@partfeatures = ();
+			}
+		}
+
+	}
+}
+
+my @keys = sort { $features{$a} <=> $features{$b} } keys(%features);
+print "Ranking:\n";
+for my $key ( @keys ) {
+	print "$key --- $features{$key}\n";
+}
