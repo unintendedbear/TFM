1fab72257df6066dca7146c4f2ea88d7596bb9fd
unintendedbear <unintendedbear@gmail.com>
2014-12-10 14:03:44 +0100

    Arregladas un par de cosillas de FeatureExtraction.pl

--- Perl/FeatureExtraction.pl
+++ Perl/FeatureExtraction.pl
@@ -39,8 +39,8 @@ for my $rule ( @rules ) {
 				my @components = split (/\sand\s/, $rule);
 				my $weight;
 				my @subcomponents = split (/\s/, $components[$#components]);
-				if ( $subcomponents[$#subcomponents] =~ /\((\d+)\.\d+\/\d+\.\d+\)/ ) {
-					$weight = $1/$instances;
+				if ( $subcomponents[$#subcomponents] =~ /\((\d+)\.\d+\/(\d+)\.\d+\)/ ) {
+					if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 				}
 				for my $component ( @components ) {
 					if ( $component =~ /^\((\w+)[\s\>\=\<]+[\w\.]+\)/ ) {
@@ -56,10 +56,11 @@ for my $rule ( @rules ) {
 				push (@partfeatures, $1);
 			}
 
-			if ( $rule =~ /^(\w+)[\s\>\=\<]+\w+\:\s\w+\s\((\d+)\.\d+\/?\d*\.*\d*\)$/ ) {
+			if ( $rule =~ /^(\w+)[\s\>\=\<]+\w+\:\s\w+\s\((\d+)\.\d+\/?(\d*)\.*\d*\)$/ ) {
 				print "$rule\n";
 				push (@partfeatures, $1);
-				my $weight = $2/$instances;
+				my $weight;
+				if ($3 eq '') { $weight = $2/$instances; } else { $weight = ($2-$3)/$instances; }
 
 				for my $i ( @partfeatures ) {
 					print "$i,";
@@ -86,8 +87,9 @@ for my $rule ( @rules ) {
 					if ( $#rulediscovery > $order+2) {
 						# Caso
 						# url_core = windowsupdate: allow (865.5)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
-						my $weight = $1/$instances;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						$features{$rulediscovery[$order]} += $weight;
 					} else {
 						# Caso
@@ -101,7 +103,7 @@ for my $rule ( @rules ) {
 						# Caso
 						# url_core = doubleclick
 						# |   content_type_MCT = application: allow (1.07)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
 						if ( $#j48rule == $order ) {
 							pop @j48rule;
 							push (@j48rule, $rulediscovery[$order]);
@@ -114,7 +116,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@j48rule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @j48rule ) {
 							$features{$k} += $weight;
 						}
@@ -143,7 +146,7 @@ for my $rule ( @rules ) {
 						# url_core = doubleclick
 						# |   num_subdomains <= 1
 						# |   |   content_type_MCT = application: allow (1.07)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
 						if ( $#j48rule == $order ) {
 							pop @j48rule;
 							push (@j48rule, $rulediscovery[$order]);
@@ -156,7 +159,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@j48rule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @j48rule ) {
 							$features{$k} += $weight;
 						}
@@ -187,7 +191,7 @@ for my $rule ( @rules ) {
 						# |   num_subdomains > 1
 						# |   |   digits_in_URL <= 16
 						# |   |   |   subdomain2 = au: allow (0.0)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
 						if ( $#j48rule == $order ) {
 							pop @j48rule;
 							push (@j48rule, $rulediscovery[$order]);
@@ -200,7 +204,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@j48rule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @j48rule ) {
 							$features{$k} += $weight;
 						}
@@ -233,7 +238,7 @@ for my $rule ( @rules ) {
 						# |   |   digits_in_URL <= 16
 						# |   |   |   subdomain2 = ad
 						# |   |   |   |   content_type_MCT = application: deny (6.0)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
 						if ( $#j48rule == $order ) {
 							pop @j48rule;
 							push (@j48rule, $rulediscovery[$order]);
@@ -246,7 +251,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@j48rule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @j48rule ) {
 							$features{$k} += $weight;
 						}
@@ -281,7 +287,7 @@ for my $rule ( @rules ) {
 						# |   |   |   bytes > 601
 						# |   |   |   |   http_reply_code = 200
 						# |   |   |   |   |   digits_in_URL <= 81: allow (182.0/3.0)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
 						if ( $#j48rule == $order ) {
 							pop @j48rule;
 							push (@j48rule, $rulediscovery[$order]);
@@ -294,7 +300,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@j48rule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @j48rule ) {
 							$features{$k} += $weight;
 						}
@@ -331,7 +338,7 @@ for my $rule ( @rules ) {
 						# |   |   |   |   http_reply_code = 200
 						# |   |   |   |   |   digits_in_URL > 81
 						# |   |   |   |   |   |   bytes <= 973: allow (4.08)
-						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+4] =~ /\((\d+)\.\d+\/?(\d*)\.*\d*\)/;
 						if ( $#j48rule == $order ) {
 							pop @j48rule;
 							push (@j48rule, $rulediscovery[$order]);
@@ -344,7 +351,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@j48rule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @j48rule ) {
 							$features{$k} += $weight;
 						}
@@ -371,8 +379,9 @@ for my $rule ( @rules ) {
 					if ( $#rulediscovery > $order+2) {
 						# Caso
 						# url_core = windowsupdate : allow (581.33/0) [284.18/0]
-						$rulediscovery[$order+5] =~ /\((\d+)\.?\d*\/?\d*\.*\d*\)/;
-						my $weight = $1/$instances;
+						$rulediscovery[$order+5] =~ /\((\d+)\.?\d*\/?(\d*)\.*\d*\)/;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						$features{$rulediscovery[$order]} += $weight;
 					} else {
 						# Caso
@@ -386,7 +395,7 @@ for my $rule ( @rules ) {
 						# Caso
 						# url_core = doubleclick
 						# |   time = 08:30:08 : deny (0/0) [0/0]
-						$rulediscovery[$order+5] =~ /\((\d+)\.?\d*\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+5] =~ /\((\d+)\.?\d*\/?(\d*)\.*\d*\)/;
 						if ( $#reprule == $order ) {
 							pop @reprule;
 							push (@reprule, $rulediscovery[$order]);
@@ -399,7 +408,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@reprule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @reprule ) {
 							$features{$k} += $weight;
 						}
@@ -428,7 +438,7 @@ for my $rule ( @rules ) {
 						# url_core = doubleclick
 						# |   time = 09:58:23
 						# |   |   http_reply_code = 204 : allow (0/0) [0/0]
-						$rulediscovery[$order+5] =~ /\((\d+)\.?\d*\/?\d*\.*\d*\)/;
+						$rulediscovery[$order+5] =~ /\((\d+)\.?\d*\/?(\d*)\.*\d*\)/;
 						if ( $#reprule == $order ) {
 							pop @reprule;
 							push (@reprule, $rulediscovery[$order]);
@@ -441,7 +451,8 @@ for my $rule ( @rules ) {
 						} else {
 							push (@reprule, $rulediscovery[$order]);
 						}
-						my $weight = $1/$instances;
+						my $weight;
+						if ($2 eq '') { $weight = $1/$instances; } else { $weight = ($1-$2)/$instances; }
 						for my $k ( @reprule ) {
 							$features{$k} += $weight;
 						}
@@ -455,7 +466,10 @@ for my $rule ( @rules ) {
 }
 
 my @keys = sort { $features{$a} <=> $features{$b} } keys(%features);
-print "Ranking:\n";
+my $outputfile = "Output.csv";
+open (OUT, ">$outputfile") or die "No existe el fichero ".$outputfile;
+print OUT "Feature,$classifier\n";
 for my $key ( @keys ) {
-	print "$key --- $features{$key}\n";
+	print OUT "$key,$features{$key}\n";
 }
+close OUT;
